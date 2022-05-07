1ebbc56076c6afc7b851b96439168c8018f94a12
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-05-07 11:12:59 +0900

    106

--- app/src/main/java/com/websarve/wings/android/myquizapp/Constants.kt
+++ app/src/main/java/com/websarve/wings/android/myquizapp/Constants.kt
@@ -96,15 +96,15 @@ object Constants {
 
         questionsList.add(que9)
 
-        // 10
-        val que10 = Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
-        )
-
-        questionsList.add(que10)
+//        // 10
+//        val que10 = Question(
+//            10, "What country does this flag belong to?",
+//            R.drawable.ic_flag_of_kuwait,
+//            "Kuwait", "Jordan",
+//            "Sudan", "Palestine", 1
+//        )
+//
+//        questionsList.add(que10)
 
         return questionsList
     }
